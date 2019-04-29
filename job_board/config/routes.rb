db8839b553dc80dc4e1ac179d2f9e91d4cacaed7
Gandhali Keskar <gkeskar@zendesk.com>
2019-04-28 21:53:20 -0700

    adding bootstrap styling

--- job_board/config/routes.rb
+++ job_board/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home', to: 'jobs#index'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'tags/:tag', to: 'jobs#index', as: :tag
   resources :jobs
